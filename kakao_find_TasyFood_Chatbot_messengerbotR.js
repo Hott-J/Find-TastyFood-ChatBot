5dd8c7756c8475a39615a42f45c4e9d1e5731c3a
Hott-J <jhj13062004@naver.com>
2020-07-28 19:24:01 +0900

    finish

--- kakao_find_TasyFood_Chatbot_messengerbotR.js
+++ kakao_find_TasyFood_Chatbot_messengerbotR.js
@@ -24,7 +24,7 @@ const Kakao=new kalingModule;
 function kakao_login(){
   try{
     Kakao.init('2d0e4efc34993dc5353ebb2f964f3f4d'); // 중요포인트 : 반드시 봇계정 카카오아이디와 패스워드로 카카오디벨로퍼에 로그인하여 자바스크립트 키값을 받아올것!
-    Kakao.login('jhj07152019@gmail.com','rkdmfdl.7');//중요포인트 : 반드시 봇계정 카카오아이디와 패스워드를 적어줄것!!
+    Kakao.login('jhj07152019@gmail.com','rkdmfdl.7');// 중요포인트 : 반드시 봇계정 카카오아이디와 패스워드를 적어줄것!!
     doc = Jsoup.connect("https://www.mangoplate.com/search/"+search).get();
   }catch(e){replier.reply("로그인 세션이 만료되었습니다.")}
 }
